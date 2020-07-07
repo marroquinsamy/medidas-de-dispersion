3d7cd1a7202a1c9185042ce9416d3210ec0d80e1
Steven Samuel Marroquín González <samy23033@gmail.com>
2020-07-07 14:26:09 -0600

    Función 1 funcional, todo funciona bien y hasta el momento la mayoría de errores están solucionados.

--- DM.py
+++ DM.py
@@ -1,49 +1,70 @@
-flag = 0
-
-def validarDatos(value, index, flag):
+# Función que valida únicamente los datos para el cálculo de las operaciones
+def validarDatos(value, index):
   while True:
     try:
-      value = int(value)
+      value = float(value)
       return value
     except ValueError:
-      if flag == 1:
-        value = input(f"ERROR: Opción no válida ({'Enter' if value == '' else value}); inténtalo de nuevo: ")
-        for 
-      elif flag == 2:
-        value = input(f"\nERROR: Dato no soportado ({'Enter' if value == '' else value}), por favor reemplázalo; posición {index + 1} en los datos que ingresaste: ")
+      value = input(f"\nERROR: dato no soportado ({'Enter' if value == '' else value}), por favor reemplázalo; posición {index + 1} en los datos que ingresaste: ")
 
+# Se añadió este arreglo para que sea más fácil añadir las opciones por escrito
 options = [
   "Desviación media para una serie de datos",
   "Desviación media para datos agrupados en una distribución de frecuencias simple"
 ]
 
-print("\nBienvenido.")
-print("\nMENÚ DE OPCIONES:")
-for index, value in enumerate(options):
-  print(f"{index + 1}. {value}.")
-selection = input("Elige la opción que desees ejecutar: ")
-selection = validarDatos(selection, 0, 1)
-
-if selection == 1:
-  mediaAritmetica = 0
-  dm = 0
-  data = []
-  print(f"\nINSTRUCCIONES DE USO:\nIngresa todos los datos separados por UN espacio, para finalizar presiona Enter.")
-  preData = input("Datos: ")
-  preData = preData.split(" ")
-  # Validación de datos
-  for index, value in enumerate(preData):
-    data.append(validarDatos(value, index, 2))
-  
-  # Cálculo de la media aritmética
-  for index, value in enumerate(data):
-    if value != "":
-      mediaAritmetica += value
-    else:
+selection = ""
+while selection != "#":
+  print("\nBienvenido.")
+  print("\nMENÚ DE OPCIONES:")
+  for index, value in enumerate(options):
+    print(f"{index + 1}. {value}.")
+  print(f"#. Salir")
+  selection = input("Elige la opción que desees ejecutar: ")
+  # Validación del ingreso para seleccionar opciones del menú
+  while True:
+    try:
+      if selection == "#":
+        break
+      selection = int(selection)
+      while selection > len(options):
+        selection = int(input(f"\nERROR: Opción inexistente. Revisa las opciones disponibles e inténtalo de nuevo: "))
       break
-  mediaAritmetica = mediaAritmetica / (index + 1)
-  print(mediaAritmetica)
-  
-  # Cálculo de la desviación mediaAritmetica
-  for index, value in enumerate(data):
-    dm += abs(value - mediaAritmetica)
+    except ValueError:
+      selection = input(f"\nERROR: dato incorrecto ({'Enter' if selection == '' else selection}), inténtalo de nuevo: ")
+
+  if selection == 1:
+    mediaAritmetica = 0
+    desviacionMedia = 0
+    data = []
+    print(f"\nINSTRUCCIONES DE USO:\nIngresa todos los datos separados por UN espacio, para finalizar presiona Enter.")
+    preData = input("Datos: ")
+    preData = preData.split(" ")
+    # Validación de datos
+    dataNumber = 0
+    for dataNumber, value in enumerate(preData):
+      data.append(validarDatos(value, dataNumber))
+    dataNumber += 1
+    
+    # Cálculo de la media aritmética
+    for index, value in enumerate(data):
+      if value != "":
+        mediaAritmetica += value
+      else:
+        break
+    mediaAritmetica = mediaAritmetica / (index + 1)
+    
+    # Cálculo de la desviación mediaAritmetica
+    for index, value in enumerate(data):
+      desviacionMedia += abs(value - mediaAritmetica)
+    desviacionMedia = desviacionMedia / dataNumber
+
+    # Presentación de resultados
+    print()
+    print(f"Desviación media: {desviacionMedia}.")
+    print(f"Media aritmética: {mediaAritmetica}.")
+    print(f"Número de datos: {dataNumber}.")
+    input()
+else:
+  print(f"\nGracias por usar mi programa :D, vuelve pronto.\nDesarrollado y escrito por Samuel Marroquín G.")
+  input("Presiona Enter para salir.")
